deb5fb953d9c83d30d64259e5d4316e680a630b8
sWintermoor <sWintermoor@gmail.com>
2025-07-09 22:49:46 +0200

    fixed camera

--- mujoco_playground/_src/locomotion/wolfgang/randomize.py
+++ mujoco_playground/_src/locomotion/wolfgang/randomize.py
@@ -33,14 +33,14 @@ def domain_randomize(model: mjx.Model, rng: jax.Array):
     # Scale static friction: *U(0.9, 1.1).
     rng, key = jax.random.split(rng)
     frictionloss = model.dof_frictionloss[6:] * jax.random.uniform(
-        key, shape=(20,), minval=0.9, maxval=1.1
+        key, shape=(18,), minval=0.9, maxval=1.1
     )
     dof_frictionloss = model.dof_frictionloss.at[6:].set(frictionloss)
 
     # Scale armature: *U(1.0, 1.05).
     rng, key = jax.random.split(rng)
     armature = model.dof_armature[6:] * jax.random.uniform(
-        key, shape=(20,), minval=1.0, maxval=1.05
+        key, shape=(18,), minval=1.0, maxval=1.05
     )
     dof_armature = model.dof_armature.at[6:].set(armature)
 
@@ -63,7 +63,7 @@ def domain_randomize(model: mjx.Model, rng: jax.Array):
     qpos0 = model.qpos0
     qpos0 = qpos0.at[7:].set(
         qpos0[7:]
-        + jax.random.uniform(key, shape=(20,), minval=-0.05, maxval=0.05)
+        + jax.random.uniform(key, shape=(18,), minval=-0.05, maxval=0.05)
     )
 
     return (
